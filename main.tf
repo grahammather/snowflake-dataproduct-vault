dd6fe212f896f063c8e07b500d0da8e62ec9d623
Graham Mather <gmather@gmail.com>
2025-10-28 17:12:43 -0600

    this demo is not cli-driven

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 terraform {
-  cloud {
-    organization = "gmather-org"
-    workspaces {
-      name = "snowflake-dataproduct-vault"
-    }
-  }
   required_providers {
     snowflake = {
       source = "snowflakedb/snowflake"
